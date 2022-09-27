f6e766a99ba6f078b3b00c19e54e2bdb95814e57
ark1987s <arkadiy1987@yandex.ru>
2022-09-27 23:51:06 +0300

    *

---
+++ Lab04.Ex06.DataViewExample/Form1.Designer.cs
@@ -0,0 +1,179 @@
+﻿namespace Lab04.Ex06.DataViewExample
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.CustomersGrid = new System.Windows.Forms.DataGridView();
+            this.SortTextBox = new System.Windows.Forms.TextBox();
+            this.FilterTextBox = new System.Windows.Forms.TextBox();
+            this.SetDataViewPropertiesButton = new System.Windows.Forms.Button();
+            this.AddRowButton = new System.Windows.Forms.Button();
+            this.northwindDataSet = new Lab04.Ex06.DataViewExample.NorthwindDataSet();
+            this.customersBindingSource = new System.Windows.Forms.BindingSource(this.components);
+            this.customersTableAdapter = new Lab04.Ex06.DataViewExample.NorthwindDataSetTableAdapters.CustomersTableAdapter();
+            this.ordersBindingSource = new System.Windows.Forms.BindingSource(this.components);
+            this.ordersTableAdapter = new Lab04.Ex06.DataViewExample.NorthwindDataSetTableAdapters.OrdersTableAdapter();
+            this.GetOrdersButton = new System.Windows.Forms.Button();
+            this.OrdersGrid = new System.Windows.Forms.DataGridView();
+            ((System.ComponentModel.ISupportInitialize)(this.CustomersGrid)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.northwindDataSet)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.customersBindingSource)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.ordersBindingSource)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.OrdersGrid)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // CustomersGrid
+            // 
+            this.CustomersGrid.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left)));
+            this.CustomersGrid.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.CustomersGrid.Location = new System.Drawing.Point(12, 12);
+            this.CustomersGrid.Name = "CustomersGrid";
+            this.CustomersGrid.Size = new System.Drawing.Size(400, 495);
+            this.CustomersGrid.TabIndex = 0;
+            // 
+            // SortTextBox
+            // 
+            this.SortTextBox.Location = new System.Drawing.Point(584, 53);
+            this.SortTextBox.Name = "SortTextBox";
+            this.SortTextBox.Size = new System.Drawing.Size(100, 20);
+            this.SortTextBox.TabIndex = 1;
+            this.SortTextBox.Text = "CustomerID";
+            // 
+            // FilterTextBox
+            // 
+            this.FilterTextBox.Location = new System.Drawing.Point(584, 112);
+            this.FilterTextBox.Name = "FilterTextBox";
+            this.FilterTextBox.Size = new System.Drawing.Size(100, 20);
+            this.FilterTextBox.TabIndex = 2;
+            this.FilterTextBox.Text = "City = \'London\'";
+            // 
+            // SetDataViewPropertiesButton
+            // 
+            this.SetDataViewPropertiesButton.Location = new System.Drawing.Point(584, 197);
+            this.SetDataViewPropertiesButton.Name = "SetDataViewPropertiesButton";
+            this.SetDataViewPropertiesButton.Size = new System.Drawing.Size(100, 23);
+            this.SetDataViewPropertiesButton.TabIndex = 3;
+            this.SetDataViewPropertiesButton.Text = "SetDataViewProperties";
+            this.SetDataViewPropertiesButton.UseVisualStyleBackColor = true;
+            this.SetDataViewPropertiesButton.Click += new System.EventHandler(this.SetDataViewPropertiesButton_Click);
+            // 
+            // AddRowButton
+            // 
+            this.AddRowButton.Location = new System.Drawing.Point(584, 271);
+            this.AddRowButton.Name = "AddRowButton";
+            this.AddRowButton.Size = new System.Drawing.Size(100, 23);
+            this.AddRowButton.TabIndex = 4;
+            this.AddRowButton.Text = "Add Row";
+            this.AddRowButton.UseVisualStyleBackColor = true;
+            this.AddRowButton.Click += new System.EventHandler(this.AddRowButton_Click);
+            // 
+            // northwindDataSet
+            // 
+            this.northwindDataSet.DataSetName = "NorthwindDataSet";
+            this.northwindDataSet.SchemaSerializationMode = System.Data.SchemaSerializationMode.IncludeSchema;
+            // 
+            // customersBindingSource
+            // 
+            this.customersBindingSource.DataMember = "Customers";
+            this.customersBindingSource.DataSource = this.northwindDataSet;
+            // 
+            // customersTableAdapter
+            // 
+            this.customersTableAdapter.ClearBeforeFill = true;
+            // 
+            // ordersBindingSource
+            // 
+            this.ordersBindingSource.DataMember = "Orders";
+            this.ordersBindingSource.DataSource = this.northwindDataSet;
+            // 
+            // ordersTableAdapter
+            // 
+            this.ordersTableAdapter.ClearBeforeFill = true;
+            // 
+            // GetOrdersButton
+            // 
+            this.GetOrdersButton.Location = new System.Drawing.Point(584, 347);
+            this.GetOrdersButton.Name = "GetOrdersButton";
+            this.GetOrdersButton.Size = new System.Drawing.Size(100, 23);
+            this.GetOrdersButton.TabIndex = 5;
+            this.GetOrdersButton.Text = "Get Orders";
+            this.GetOrdersButton.UseVisualStyleBackColor = true;
+            this.GetOrdersButton.Click += new System.EventHandler(this.GetOrdersButton_Click);
+            // 
+            // OrdersGrid
+            // 
+            this.OrdersGrid.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.OrdersGrid.Location = new System.Drawing.Point(830, 12);
+            this.OrdersGrid.Name = "OrdersGrid";
+            this.OrdersGrid.Size = new System.Drawing.Size(419, 495);
+            this.OrdersGrid.TabIndex = 6;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1261, 519);
+            this.Controls.Add(this.OrdersGrid);
+            this.Controls.Add(this.GetOrdersButton);
+            this.Controls.Add(this.AddRowButton);
+            this.Controls.Add(this.SetDataViewPropertiesButton);
+            this.Controls.Add(this.FilterTextBox);
+            this.Controls.Add(this.SortTextBox);
+            this.Controls.Add(this.CustomersGrid);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.CustomersGrid)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.northwindDataSet)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.customersBindingSource)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.ordersBindingSource)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.OrdersGrid)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.DataGridView CustomersGrid;
+        private System.Windows.Forms.TextBox SortTextBox;
+        private System.Windows.Forms.TextBox FilterTextBox;
+        private System.Windows.Forms.Button SetDataViewPropertiesButton;
+        private System.Windows.Forms.Button AddRowButton;
+        private NorthwindDataSet northwindDataSet;
+        private System.Windows.Forms.BindingSource customersBindingSource;
+        private NorthwindDataSetTableAdapters.CustomersTableAdapter customersTableAdapter;
+        private System.Windows.Forms.BindingSource ordersBindingSource;
+        private NorthwindDataSetTableAdapters.OrdersTableAdapter ordersTableAdapter;
+        private System.Windows.Forms.Button GetOrdersButton;
+        private System.Windows.Forms.DataGridView OrdersGrid;
+    }
+}
+
---
+++ Lab04.Ex06.DataViewExample/Form1.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Lab04.Ex06.DataViewExample
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        DataView customersDataView;
+        DataView ordersDataView;
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            // TODO: данная строка кода позволяет загрузить данные в таблицу "northwindDataSet.Orders". При необходимости она может быть перемещена или удалена.
+            this.ordersTableAdapter.Fill(this.northwindDataSet.Orders);
+            // TODO: данная строка кода позволяет загрузить данные в таблицу "northwindDataSet.Customers". При необходимости она может быть перемещена или удалена.
+            this.customersTableAdapter.Fill(this.northwindDataSet.Customers);
+            customersTableAdapter.Fill(northwindDataSet.Customers);
+            ordersTableAdapter.Fill(northwindDataSet.Orders);
+            customersDataView = new DataView(northwindDataSet.Customers);
+            ordersDataView = new DataView(northwindDataSet.Orders);
+            customersDataView.Sort = "CustomerID";
+            CustomersGrid.DataSource = customersDataView;
+        }
+
+        private void SetDataViewPropertiesButton_Click(object sender, EventArgs e)
+        {
+            customersDataView.Sort = SortTextBox.Text;
+            customersDataView.RowFilter = FilterTextBox.Text;
+        }
+
+        private void AddRowButton_Click(object sender, EventArgs e)
+        {
+            DataRowView newCustomRow = customersDataView.AddNew();
+            newCustomRow["CustomerID"] = "WINGT";
+            newCustomRow["CompanyName"] = "Wing Tip Toys";
+            newCustomRow.EndEdit();
+        }
+
+        private void GetOrdersButton_Click(object sender, EventArgs e)
+        {
+            string selectedCustomerID = (string)CustomersGrid.SelectedCells[0].OwningRow.Cells["CustomerID"].Value;
+            DataRowView selectedRow = customersDataView[customersDataView.Find(selectedCustomerID)];
+            ordersDataView = selectedRow.CreateChildView(northwindDataSet.Relations["FK_Orders_Customers"]);
+            OrdersGrid.DataSource = ordersDataView;
+        }
+    }
+}
