4a8656e476815a6d9e9bef532bf836d51c7c1eae
ark1987s <arkadiy1987@yandex.ru>
2022-10-10 12:22:11 +0300

    update

--- ControlWork/Main.cs
+++ ControlWork/Main.cs
@@ -13,28 +13,39 @@ using System.Windows.Forms;
 namespace ControlWork
 {    
     public partial class Main : Form
-    {        
+    {    
+        private SqlConnection ApressFinancialConn = new SqlConnection($"Data Source=.\\SQLEXPRESS;Initial Catalog=ApressFinancial;Persist Security Info=True;User ID={Data.login};Password={Data.password}");
         public Main()
         {           
-            InitializeComponent();
-            sqlConnection1.ConnectionString = $"Data Source=.\\SQLEXPRESS;Initial Catalog=ApressFinancial;Persist Security Info=True;User ID={Data.login};Password={Data.password}";
-            
+            InitializeComponent();                     
         }
+        private SqlDataAdapter SqlDataAdapterAF;
+        private DataSet AFDataset = new DataSet("ApressFinancial");
+        private DataTable CustomersTable = new DataTable("Customers");
+
+
         private void Form1_Load(object sender, EventArgs e)
         {
-            dataGridView1.DataSource = apressFinancialDataSet.Customers;
-            sqlDataAdapter1.Fill(apressFinancialDataSet.Customers);
+            SqlDataAdapterAF = new SqlDataAdapter("SELECT * FROM [ApressFinancial].[CustomerDetails].[Customers]", ApressFinancialConn);
+            AFDataset.Tables.Add(CustomersTable);
+            SqlDataAdapterAF.Fill(AFDataset.Tables["Customers"]);
+            dataGridView1.DataSource = AFDataset.Tables["Customers"];
+            SqlCommandBuilder commands = new SqlCommandBuilder(SqlDataAdapterAF);
         }
+
+
+
         private void delete_Click(object sender, EventArgs e)
         {
-            foreach(DataGridViewRow row in dataGridView1.SelectedRows)
+            foreach (DataGridViewRow row in dataGridView1.SelectedRows)
             {
                 dataGridView1.Rows.Remove(row);
             }
         }
         private void Save_Click(object sender, EventArgs e)
         {
-            sqlDataAdapter1.Update(apressFinancialDataSet);
+            AFDataset.EndInit();
+            SqlDataAdapterAF.Update(AFDataset.Tables["Customers"]);
         }
     }
 }
---
+++ Lab09.Ex01.CodeFirst/Properties/AssemblyInfo.cs
@@ -0,0 +1,36 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// Общие сведения об этой сборке предоставляются следующим набором
+// набора атрибутов. Измените значения этих атрибутов для изменения сведений,
+// связанные со сборкой.
+[assembly: AssemblyTitle("Lab09.Ex01.CodeFirst")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("Lab09.Ex01.CodeFirst")]
+[assembly: AssemblyCopyright("Copyright ©  2022")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Установка значения False для параметра ComVisible делает типы в этой сборке невидимыми
+// для компонентов COM. Если необходимо обратиться к типу в этой сборке через
+// COM, задайте атрибуту ComVisible значение TRUE для этого типа.
+[assembly: ComVisible(false)]
+
+// Следующий GUID служит для идентификации библиотеки типов, если этот проект будет видимым для COM
+[assembly: Guid("b74f64fc-a619-4817-b6e7-09dd4a9cc620")]
+
+// Сведения о версии сборки состоят из указанных ниже четырех значений:
+//
+//      Основной номер версии
+//      Дополнительный номер версии
+//      Номер сборки
+//      Редакция
+//
+// Можно задать все значения или принять номера сборки и редакции по умолчанию 
+// используя "*", как показано ниже:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
